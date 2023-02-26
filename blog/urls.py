7c3ad9dcd0a2e1400230dcefea6de4310be06d01
juancazenave <juanfranciscocazenave@gmail.com>
2023-02-26 22:36:51 +0000

    26022023 fin del dia

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', base, name='base')
+    path('', base, name='base'),
+    path('homepage/', homepage, name='homepage'),
+    path('<slug:slug>/', post_detail, name='post_detail')
 ]
