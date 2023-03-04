483527a47d7e77983729e1145100732c266442bb
juancazenave <juanfranciscocazenave@gmail.com>
2023-03-04 17:39:49 +0000

    04032023 2

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ from .views import *
 urlpatterns = [
     path('', base, name='base'),
     path('homepage/', homepage, name='homepage'),
-    path('post/<slug:slug>/', post_detail, name='post_detail'),
+    path('post/<id>/', post_detail, name='post_detail'),
     path('create-post/', create_post, name='create_post')
 ]
--- blog/views.py
+++ blog/views.py
@@ -11,8 +11,8 @@ def homepage(request):
     posts = Post.objects.all()
     return render(request, 'blog/homepage.html', {'posts':posts})
 
-def post_detail(request, slug):
-    post = Post.objects.get(slug=slug)
+def post_detail(request, id):
+    post = Post.objects.get(id=id)
     return render(request, 'blog/post_detail.html', {'post':post})
 
 def create_post(request):
